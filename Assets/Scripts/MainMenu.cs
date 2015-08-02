9d4ede1b0873ddfef1435cd1e76b9261aa9b63b9
Khetonah <erdetyrenne@gmail.com>
2015-08-02 11:29:33 +0200

    Ajout Menu element et effets

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string startLevel;
+	// Use this for initialization
+	public void NewGame() 
+	{
+		Application.LoadLevel (startLevel);
+	}
+	
+	// Update is called once per frame
+	public void QuitGame() 
+	{
+		Application.Quit ();
+	}
+}
