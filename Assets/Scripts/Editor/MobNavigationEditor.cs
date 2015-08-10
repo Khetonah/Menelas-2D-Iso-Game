fa443fde4c75f66ad9d1cc06b22f0fcc8ea16972
elvoniel <bedu.william@gmail.com>
2015-08-10 21:48:45 +0200

    Script MobNavigation : dplacement alatorie en cercle

---
+++ Assets/Scripts/Editor/MobNavigationEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MobNavigation))]
+public class MobNavigationEditor : Editor {
+
+	private MobNavigation mn;
+	private Vector3 initPos;
+
+	void OnEnable() {
+		mn = (MobNavigation)target;
+		initPos = mn.transform.position;
+
+	}
+
+	void OnSceneGUI() {
+		if (mn.movingType == MobNavigation.MovingType.randomCircular) {
+
+			float range = mn.range;
+
+			Handles.color = new Color ((float)1, (float)0, (float)0, (float)0.1);
+			Handles.DrawSolidDisc (initPos, Vector3.up, range);
+			Handles.color = Color.red;
+			range = 
+				Handles.ScaleValueHandle (range,
+				                          initPos + new Vector3 (range, 0, 0),
+				                         Quaternion.identity,
+				                         2,
+				                         Handles.CylinderCap,
+				                         2);
+		}
+	}
+
+}
--- Assets/Scripts/MobNavigation.cs
+++ Assets/Scripts/MobNavigation.cs
@@ -8,13 +8,15 @@ public class MobNavigation : MonoBehaviour {
 
 	public enum MovingType
 	{
-		randomRange,
+		randomCircular,
 		path
 	};
 
 	public MovingType movingType;
 	public float range = 0;
+	public float sleepTime = 1;
 
+	private float time = 0;
 	private Vector3 origin;
 	private NavMeshAgent navAgent;
 
@@ -29,26 +31,24 @@ public class MobNavigation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
 		// Check if we've reached the destination
-		if (!navAgent.pathPending)
-		{
-			if (navAgent.remainingDistance <= navAgent.stoppingDistance)
-			{
-				if (!navAgent.hasPath || navAgent.velocity.sqrMagnitude == 0f)
-				{
-					float angle = UnityEngine.Random.Range ((float) 0.0, (float) (2 * Math.PI));
+		if (!navAgent.pathPending) {
+			if (navAgent.remainingDistance <= navAgent.stoppingDistance) {
+				if (!navAgent.hasPath || navAgent.velocity.sqrMagnitude == 0f) {
+					time += Time.deltaTime;
+					if (time >= sleepTime) {
 
-					if (movingType == MovingType.randomRange) {
-						navAgent.SetDestination(new Vector3(origin.x + (float)(Math.Cos(angle) * range) ,origin.y, (float) origin.z + ((float)UnityEngine.Random.Range((float)-Math.Sin(angle), (float)Math.Sin(angle)) * range)));
+						if (movingType == MovingType.randomCircular) {
+							float angle = UnityEngine.Random.Range ((float)0.0, (float)(2 * Math.PI));
+
+							navAgent.SetDestination (new Vector3 (origin.x + (float)(Math.Cos (angle) * range), origin.y, (float)origin.z + ((float)UnityEngine.Random.Range ((float)-Math.Sin (angle), (float)Math.Sin (angle)) * range)));
+						}
+						time = 0;
 					}
+
 				}
 			}
-		}
-
-		//Debug.Log ("Titi = " + Math.Cos (Random.Range(0, 2 * Math.PI)));
-		Debug.Log("Range : " + UnityEngine.Random.Range ((float) 0.0, (float) (2 * Math.PI)));
-
+		}	
 
 	}
 }
